d4a5ac6bc0ed28975a74ddbbe632e3e2d58abdae
NermineAli1995 <nermine.ali@esprit.tn>
2021-04-27 23:06:29 +0100

    add method fav_tweet

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,11 +1,14 @@
 import tweepy
 import os
+import
 from os import environ
 
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
+
+FILENAME_FAV = "id_favorite_tweet.txt" 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -22,3 +25,20 @@ def store_last_seen_id(last_seen_id,file_name):
   f_write.write(str(last_seen_id))
   f_write.close()
   return
+
+def fav_tweet():
+  last_seen_id = retrieve_last_seen_id(FILE_NAME_FAV)
+  mentions = api.mentions_timeline(last_seen_id,tweet_mode='extended')
+  for mention in reversed(mentions):
+    if not mention:
+      return
+    print(str(mention.id)+' - '+mention.full_text,flush=True)
+    last_fav_tweet = mention.id
+    store_last_seen_id(last_fav_tweet,FILE_NAME_FAV)
+    api.create_favorite(mention.id)
+    api.retweet(mention.id)
+
+
+while True:
+  fav_tweet()
+  time.sleep(15)
